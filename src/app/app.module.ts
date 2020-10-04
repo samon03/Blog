bcd08ab1572c0779b975399c7fd59213f7a1c5d5
samon03 <samon3123@gmail.com>
2020-10-04 17:45:45 +0600

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticalComponent } from './artical/artical.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ArticalComponent
   ],
   imports: [
     BrowserModule
