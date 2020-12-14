e0fa2e142a6bc750bd5d20b94396648d9d763b9b
Samon03 <samon3123@gmail.com>
2020-12-14 17:10:09 +0600

    Final

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/Blog/'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/Blog/index.html'));});
-app.listen(process.env.PORT || 3000);
